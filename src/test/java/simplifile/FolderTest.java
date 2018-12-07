ef38968f65b7e200d2f91decea197bc3aed48bc7
Marco Fracassi <fracassi.marco@gmail.com>
2018-12-07 14:28:14 +0100

    Test cleanup

--- src/test/java/simplifile/FolderTest.java
+++ src/test/java/simplifile/FolderTest.java
@@ -1,19 +1,32 @@
 package simplifile;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FolderTest {
 
+    private Path fullPath;
+
+    @Before
+    public void setUp() throws Exception {
+        fullPath = Files.createTempDirectory("simplifile");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Files.deleteIfExists(fullPath);
+    }
+
     @Test
     public void shouldExists() throws IOException {
-        String fullPath = Files.createTempDirectory("simplifile").toString();
-
-        assertThat(new Folder(fullPath).exists()).isTrue();
+        assertThat(new Folder(fullPath.toString()).exists()).isTrue();
 
         assertThat(new Folder("/not_existing").exists()).isFalse();
     }
